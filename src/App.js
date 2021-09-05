a6a4c3a326c9c209cc4122154ce5aa3ed17024e8
rahul <esakkanavar98@gmail.com>
2021-09-06 00:00:39 +0530

    React Http

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import Greet from './Components/Getrequest';
+import Posts from './Components/Postform';
+function App() {
+  return (
+    <div className="App">
+     {/* <Greet/> */}
+     <Posts/>
+    </div>
+  );
+}
+
+export default App;
